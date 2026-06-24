fad367863c3934b177d6d9eb8d8f391c365dbf74
Roland <1124997@hr.nl>
2026-06-25 00:30:38 +0200

    fix: correct conditional structure for query handling based on user authentication

--- server/api/articles/index.get.ts
+++ server/api/articles/index.get.ts
@@ -3,8 +3,7 @@ export default defineBaseEventHandler(async (event, { server, user }) => {
 	let query;
 
 	if (!user) query = server.from("artikelen").select("*").eq("published", true).order("updated_at", { ascending: false });
-
-	if (search) query = server.rpc("search_artikelen", { search: search }).order("updated_at", { ascending: false });
+	else if (search) query = server.rpc("search_artikelen", { search: search }).order("updated_at", { ascending: false });
 	else query = server.from("artikelen").select("*").order("updated_at", { ascending: false });
 
 	const { data: articles, error } = await query;
